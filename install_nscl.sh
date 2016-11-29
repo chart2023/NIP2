21e2118fef7cfc94907b8b3a2219cc1076165e06
Apichart <chart2023@users.noreply.github.com>
2016-11-29 10:58:57 +0700

    Update install_nscl.sh

--- install_nscl.sh
+++ install_nscl.sh
@@ -10,10 +10,9 @@ ipaddress=$(head -1 $ipfile)
 bash -c "echo $ipaddress `cat /etc/hostname` >> /etc/hosts"
 wget -q --tries=10 --timeout=20 --spider  http://archive.ubuntu.com
 if [[ $? -eq 0 ]]; then
-        echo "Server is online"
+        echo "Server can connect to Internet"
 else
-        echo "Server is Offline"
-        exit 0
+        echo "Server cannot connect to Internet"
 fi
 cp /opt/openbaton/scripts/start-nscl.sh /etc/init.d/start-nscl.sh
 chmod ugo+x /etc/init.d/start-nscl.sh
