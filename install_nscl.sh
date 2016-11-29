2dadf6aa94d004158ec0c374d4bee4d424a7b8e0
Apichart <chart2023@users.noreply.github.com>
2016-11-29 10:11:17 +0700

    Update install_nscl.sh

--- install_nscl.sh
+++ install_nscl.sh
@@ -4,7 +4,7 @@ myhome=${HOME}
 log_file="$myhome/install-log.txt"
 [ -f "$log_file" ] || touch "$log_file"
 exec 1>> $log_file 2>&1
-ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' >> ./ipaddress.txt
+ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' > ./ipaddress.txt
 ipfile="./ipaddress.txt"
 ipaddress=$(head -1 $ipfile)
 bash -c "echo $ipaddress `cat /etc/hostname` >> /etc/hosts"
