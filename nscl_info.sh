b62773e92bea5a72256271ce0ecd5b67f65f181c
Apichart <chart2023@users.noreply.github.com>
2016-12-05 15:38:35 +0700

    Update nscl_info.sh

--- nscl_info.sh
+++ nscl_info.sh
@@ -4,7 +4,10 @@ echo "nscl_ip=$nscl_private" | tee --append ${HOME}/nscl_info.conf
 echo "nscl_fip=$nscl_private_floatingIp" | tee --append ${HOME}/nscl_info.conf
 echo "nscl_hostname=$nscl_hostname" | tee --append ${HOME}/nscl_info.conf
 sudo rm -rf ${HOME}/OpenMTC-Chula/openmtc/settings/ipserv.js
+#SET IP LBaaS
 echo "exports.ipnscl='$nscl_private';" | tee --append ${HOME}/OpenMTC-Chula/openmtc/settings/ipserv.js
+#Set IP NSCL
+#echo "exports.ipnscl='$nscl_private';" | tee --append ${HOME}/OpenMTC-Chula/openmtc/settings/ipserv.js
 sudo bash -c "echo exports.ipnip=\'`ifconfig eth0 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://'`\'\; >> ${HOME}/OpenMTC-Chula/openmtc/settings/ipserv.js"
 echo "exports.fipnip='$nscl_private_floatingIp';" >> tee --append ${HOME}/OpenMTC-Chula/openmtc/settings/ipserv.js
 echo "exports.ipopenstack='161.200.90.78';" >> tee --append ${HOME}/OpenMTC-Chula/openmtc/settings/ipserv.js
