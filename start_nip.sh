1bbf3540ef0a716651136ebb0ca4065e5a964a13
Apichart <chart2023@users.noreply.github.com>
2016-12-06 22:11:14 +0700

    Create start_nip.sh

---
+++ start_nip.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sleep 30
+sudo nohup sudo node ${HOME}/OpenMTC-Chula/openmtc-NIP/ProxyGateway/NIP_IEEE1888_ETSI.js >${HOME}/ieee.log 2>${HOME}/ieee.err &
+sleep 10
+sudo nohup sudo node ${HOME}/OpenMTC-Chula/openmtc-NIP/ProxyGateway/NIP_ETSI_IEEE1888_nscl.js >${HOME}/etsi.log 2>${HOME}/etsi.err &
