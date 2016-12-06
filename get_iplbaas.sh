a2fa26158a17ba26c931a0049b9c122956639daa
Apichart <chart2023@users.noreply.github.com>
2016-12-06 16:06:51 +0700

    Update get_iplbaas.sh

--- get_iplbaas.sh
+++ get_iplbaas.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 user='chart'
 host_os='192.168.9.14'
-#ssh-keygen
-#keyfile="${HOME}/.ssh/id_rsa.pub"
-#keydata=$(cat $keyfile)
-#echo $keydata
-#expect ./exchangekey.exp $keydata
+ssh-keygen
+keyfile="${HOME}/.ssh/id_rsa.pub"
+keydata=$(cat $keyfile)
+echo $keydata
+expect ./exchangekey.exp $keydata
 iplbaas=$(ssh $user@$host_os "./request_vip_lbaas.sh")
 echo $iplbaas
 echo "exports.iplbaas='$iplbaas';" | tee --append ${HOME}/OpenMTC-Chula/openmtc/settings/ipserv.js
