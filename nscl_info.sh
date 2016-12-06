711cf46d0002c6b1c821f467bf0ca0e8bbe4d7f9
Apichart <chart2023@users.noreply.github.com>
2016-12-06 16:02:50 +0700

    Update nscl_info.sh

--- nscl_info.sh
+++ nscl_info.sh
@@ -6,7 +6,7 @@ echo "nscl_hostname=$nscl_hostname" | tee --append ${HOME}/nscl_info.conf
 sudo rm -rf ${HOME}/OpenMTC-Chula/openmtc/settings/ipserv.js
 #SET IP LBaaS
 echo "exports.ipnscl='$nscl_private';" | tee --append ${HOME}/OpenMTC-Chula/openmtc/settings/ipserv.js
-echo "exports.iplbaas='10.0.14.7';" | tee --append ${HOME}/OpenMTC-Chula/openmtc/settings/ipserv.js
+#echo "exports.iplbaas='10.0.14.7';" | tee --append ${HOME}/OpenMTC-Chula/openmtc/settings/ipserv.js
 #Set IP NSCL
 #echo "exports.ipnscl='$nscl_private';" | tee --append ${HOME}/OpenMTC-Chula/openmtc/settings/ipserv.js
 sudo bash -c "echo exports.ipnip=\'`ifconfig eth0 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://'`\'\; >> ${HOME}/OpenMTC-Chula/openmtc/settings/ipserv.js"
