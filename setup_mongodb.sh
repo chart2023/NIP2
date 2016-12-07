1f7f52739e3383816e01d8c9f7cee7fa5a9efb04
Apichart <chart2023@users.noreply.github.com>
2016-12-07 15:59:36 +0700

    Update setup_mongodb.sh

--- setup_mongodb.sh
+++ setup_mongodb.sh
@@ -2,7 +2,6 @@
 NIP_START_TIME=$(date)
 MYHOME=${HOME}
 THISHOST=$(hostname)
-REPLSET=$(cat /dev/urandom | tr -dc 'A-Z' | fold -w 3 | head -n 1)
 log_file="$MYHOME/install-log.txt"
 [ -f "$log_file" ] || touch "$log_file"
 exec 1>> $log_file 2>&1
@@ -19,16 +18,4 @@ else
 fi
 ntpq -p
 cp /opt/openbaton/scripts/initshard.js ${HOME}/initshard.js
-echo $REPLSET >> ${HOME}/mongodb_info.conf
-sudo service mongod stop
-sudo rm -rf /var/lib/mongodbs
-sudo rm -rf /var/lib/mongod
-sudo mkdir /var/lib/mongodbs
-sudo mongod --configsvr --replSet configReplSet --port 27019 --dbpath /var/lib/mongodbs --fork --syslog
-sleep 3
-mongo --port 27019 --eval "rs.initiate()"
-sudo mkdir /var/lib/mongod
-sudo mongod --shardsvr --replSet $REPLSET --dbpath /var/lib/mongod --fork --syslog --port 27017
-mongo --port 27017 --eval "rs.initiate()"
-sudo mongos --configdb configReplSet/$THISHOST:27019 --port 27020 --fork --syslog
-mongo --host $THISHOST --port 27020 --eval "sh.addShard( '$REPLSET/$THISHOST:27017' )"
+
