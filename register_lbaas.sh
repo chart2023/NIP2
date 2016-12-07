7b87eaf9f8ecdcf53f23e3b13bbd219126d49f01
Apichart <chart2023@users.noreply.github.com>
2016-12-07 17:19:31 +0700

    Update register_lbaas.sh

--- register_lbaas.sh
+++ register_lbaas.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+#!/bin/bash
+user='chart'
+host_os='192.168.9.14'
+ipfile="./ipaddress.txt"
+ipaddress=$(head -1 $ipfile)
+ssh-keygen
+keyfile="${HOME}/.ssh/id_rsa.pub"
+keydata=$(cat $keyfile)
+echo $keydata
+expect ./exchangekey.exp $keydata
+ssh $user@$host_os "./add_ip_lbaas.sh $ipaddress")
