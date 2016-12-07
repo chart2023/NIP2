c2b9626304ad459977c29005a6a3679eb7a1b996
Apichart <chart2023@users.noreply.github.com>
2016-12-07 23:31:58 +0700

    Update setup_mongodb.sh

--- setup_mongodb.sh
+++ setup_mongodb.sh
@@ -7,6 +7,7 @@ echo $REPLSET >> ${HOME}/db_info.conf
 log_file="$MYHOME/install-log.txt"
 [ -f "$log_file" ] || touch "$log_file"
 exec 1>> $log_file 2>&1
+rm -rf /opt/openbaton/scripts/
 ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' > ./ipaddress.txt
 ipfile="./ipaddress.txt"
 ipaddress=$(head -1 $ipfile)
