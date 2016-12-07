7f756ba40b5eb1323a40c76a3e6c8c7521144768
Apichart <chart2023@users.noreply.github.com>
2016-12-07 16:33:29 +0700

    Update setup_nip.sh

--- setup_nip.sh
+++ setup_nip.sh
@@ -22,3 +22,8 @@ cp /opt/openbaton/scripts/start-nip.sh /etc/init.d/start-nip.sh
 chmod ugo+x /etc/init.d/start-nip.sh
 update-rc.d start-nip.sh defaults
 ntpq -p
+wget http://192.168.9.14:8080/v1/AUTH_b8e61c4a0b1b4d2f82929563cab8c55a/openmtc/openstack_key14.pem --output-document=${HOME}/openstack_key14.pem
+sudo chmod 600 ${HOME}/openstack_key14.pem
+$user='ubuntu'
+$dbhost='192.168.9.122'
+ssh -o StrictHostKeyChecking=no -i ${HOME}/openstack_key14.pem -l $user $dbhost --eval "bash /opt/openbaton/scripts/setup_shard.sh"
