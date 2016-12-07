fd91e02c504fc5a5e3ade885d4374081f316351f
Apichart <chart2023@users.noreply.github.com>
2016-12-07 17:06:28 +0700

    Create add_ip_lbaas.sh

---
+++ add_ip_lbaas.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ipaddress=$1
+source ${HOME}/devstack/openrc admin admin >/dev/null
+neutron lbaas-member-create  --subnet private-subnet --address $ipaddress --protocol-port 15000 pool1
