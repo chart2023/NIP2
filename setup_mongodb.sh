27f1d8950ee89adc984565f722bccaa5b37ce3de
Apichart <chart2023@users.noreply.github.com>
2016-12-08 14:23:55 +0700

    Update setup_mongodb.sh

--- setup_mongodb.sh
+++ setup_mongodb.sh
@@ -7,6 +7,9 @@ echo $REPLSET >> ${HOME}/db_info.conf
 log_file="$MYHOME/install-log.txt"
 [ -f "$log_file" ] || touch "$log_file"
 exec 1>> $log_file 2>&1
+dpkg-reconfigure -f noninteractive tzdata
+apt-get update
+apt-get install --reinstall tzdata -y
 rm -rf /opt/openbaton/scripts/
 ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' > ./ipaddress.txt
 ipfile="./ipaddress.txt"
