2512df68858e5f2ca786730dab138cae18669848
Apichart <chart2023@users.noreply.github.com>
2016-12-09 21:40:34 +0700

    Update client_configure.sh

--- client_configure.sh
+++ client_configure.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "nip_ip=$nip_private" | sudo tee --append ${HOME}/nip_info.conf
-echo "nip_fip=$nip_private_floatingIp" | sudo tee --append ${HOME}/nip_info.conf
-echo "nip_hostname=$nip_hostname" | sudo tee --append ${HOME}/nip_info.conf
+echo "nip_ip=$client_private" | sudo tee --append ${HOME}/nip_info.conf
+echo "nip_fip=$client_private_floatingIp" | sudo tee --append ${HOME}/nip_info.conf
+echo "nip_hostname=$client_hostname" | sudo tee --append ${HOME}/nip_info.conf
