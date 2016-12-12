30f1958d3e1f29e8e9a1b989864ef7730a3813c6
Apichart <chart2023@users.noreply.github.com>
2016-12-12 11:49:58 +0700

    Update start-nscl.sh

--- start-nscl.sh
+++ start-nscl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-$user='ubuntu'
-$dbhost='192.168.9.122'
+user='ubuntu'
+dbhost='192.168.9.122'
 wget http://192.168.9.14:8080/v1/AUTH_b8e61c4a0b1b4d2f82929563cab8c55a/openmtc/openstack_key14.pem --output-document=${HOME}/openstack_key14.pem
 sudo chmod 600 ${HOME}/openstack_key14.pem
 sudo nohup sudo node ${HOME}/OpenMTC-Chula/nscl >${HOME}/nscl.log 2>${HOME}/nscl.err &
