436338ec0bb9b05f73ee1128c6c053a523ca5b2a
Apichart <chart2023@users.noreply.github.com>
2016-12-12 18:03:32 +0700

    Update start-nscl.sh

--- start-nscl.sh
+++ start-nscl.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 user='ubuntu'
 dbhost='192.168.9.122'
-wget http://192.168.9.14:8080/v1/AUTH_b8e61c4a0b1b4d2f82929563cab8c55a/openmtc/openstack_key14.pem
+wget http://192.168.9.14:8080/v1/AUTH_b8e61c4a0b1b4d2f82929563cab8c55a/openmtc/openstack_key14.pem -o /root/openstack_key14.pem
 sudo chmod 600 ${HOME}/openstack_key14.pem
-sudo nohup sudo node /home/ubuntu/OpenMTC-Chula/nscl >/home/ubuntu/nscl.log 2>$/home/ubuntu/nscl.err &
+sudo nohup sudo node /home/ubuntu/OpenMTC-Chula/nscl >/home/ubuntu/nscl.log 2>/home/ubuntu/nscl.err &
 sleep 5
-ssh -o StrictHostKeyChecking=no -i ${HOME}/openstack_key14.pem -l $user $dbhost --eval "mongo --port 27020 initshard.js"
+ssh -o StrictHostKeyChecking=no -i /root/openstack_key14.pem -l $user $dbhost --eval "mongo --port 27020 initshard.js"
