872f71751a576ebce1d42c25237cda0013edba4c
Apichart <chart2023@users.noreply.github.com>
2016-12-12 12:43:38 +0700

    Update add-iplbaas.sh

--- add-iplbaas.sh
+++ add-iplbaas.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "add iplbaas"
 ipaddress=$1
 source ${HOME}/devstack/openrc admin admin >/dev/null
 neutron lbaas-member-create  --subnet private-subnet --address $ipaddress --protocol-port 15000 pool1
