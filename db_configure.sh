5d50c4df38bb2c1c93034f73343c1ba3ded6eb23
Apichart <chart2023@users.noreply.github.com>
2016-12-12 12:44:41 +0700

    Update db_configure.sh

--- db_configure.sh
+++ db_configure.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "db configure"
 echo "db_ip=$db_private" | sudo tee --append ${HOME}/db_info.conf
 echo "db_fip=$db_private_floatingIp" | sudo tee --append ${HOME}/db_info.conf
 echo "db_hostname=$db_hostname" | sudo tee --append ${HOME}/db_info.conf
