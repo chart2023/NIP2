ca1e0aecfd0a305f376db1b9ce2197438e74e168
Apichart <chart2023@users.noreply.github.com>
2016-12-13 18:39:35 +0700

    Update register-shard.sh

--- register-shard.sh
+++ register-shard.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 echo "STEP: REGISTER SHARD"
 $REPLSET=$(head -1 ${HOME}/db_info.conf)
-QUERY_ROUTER="192.168.9.122"
+ipfile="./ipaddress.txt"
+ipaddress=$(head -1 $ipfile)
 sudo rm -rf /var/lib/mongod
 sudo mkdir /var/lib/mongod
 sudo mongod --shardsvr --replSet $REPLSET --dbpath /var/lib/mongod --fork --syslog --port 27017
 sleep 3
 mongo --port 27017 --eval "rs.initiate()"
 sleep 3
-mongo --host $QUERY_ROUTER --port 27020 --eval "sh.addShard( '$REPLSET/$THISHOST:27017' )"
+mongo --host $ipaddress --port 27020 --eval "sh.addShard( '$REPLSET/$THISHOST:27017' )"
