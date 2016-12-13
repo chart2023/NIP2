16ff438acf2037f852b71ae992496f4a5665cdb2
Apichart <chart2023@users.noreply.github.com>
2016-12-13 18:02:41 +0700

    Update get-iplbaas.sh

--- get-iplbaas.sh
+++ get-iplbaas.sh
@@ -7,8 +7,8 @@ ssh-keygen -N "" -f /home/ubuntu/.ssh/id_rsa
 keyfile="/home/ubuntu/.ssh/id_rsa.pub"
 keydata=$(cat $keyfile)
 echo $keydata
-expect /opt/openbaton/script/exchangekey.exp $keydata
+sudo expect /opt/openbaton/script/exchangekey.exp $keydata
 iplbaas=$(ssh $user@$host_os "./request_vip_lbaas.sh")
 echo "iplbaas="
 echo $iplbaas
-echo "exports.iplbaas='$iplbaas';" | tee --append /home/ubuntu/OpenMTC-Chula/openmtc/settings/ipserv.js
+sudo echo "exports.iplbaas='$iplbaas';" | tee --append /home/ubuntu/OpenMTC-Chula/openmtc/settings/ipserv.js
