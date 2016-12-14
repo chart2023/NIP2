194a9fcec79aa2745a7d81609a7b32f4ecdf8f5b
Apichart <chart2023@users.noreply.github.com>
2016-12-14 16:35:09 +0700

    Create client_configuredb.sh

---
+++ client_configuredb.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "client configure"
+echo "nip_ip=$client_private" | sudo tee --append ${HOME}/nip_info.conf
+echo "nip_fip=$client_private_floatingIp" | sudo tee --append ${HOME}/nip_info.conf
+echo "nip_hostname=$client_hostname" | sudo tee --append ${HOME}/nip_info.conf
+echo "STEP: REGISTER SHARD"
+$REPLSET=$(head -1 ${HOME}/db_info.conf)
+ipfile="./ipaddress.txt"
+ipaddress=$(head -1 $ipfile)
+sudo rm -rf /var/lib/mongod
+sudo mkdir /var/lib/mongod
+sudo mongod --shardsvr --replSet $REPLSET --dbpath /var/lib/mongod --fork --syslog --port 27017
+sleep 3
+mongo --port 27017 --eval "rs.initiate()"
+sleep 3
+mongo --host $ipaddress --port 27020 --eval "sh.addShard( '$REPLSET/$THISHOST:27017' )"
