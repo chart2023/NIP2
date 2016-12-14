f27ef63b3b51d658c5355214e27164145fde9d8a
Apichart <chart2023@users.noreply.github.com>
2016-12-14 15:44:08 +0700

    Update server_configure.sh

--- server_configure.sh
+++ server_configure.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+ipfile="./ipaddress.txt"
+ipaddress=$(head -1 $ipfile)
 sudo rm -rf ${HOME}/nscl_info.conf
 echo "nscl_ip=$server_private" | tee --append ${HOME}/nscl_info.conf
 echo "nscl_fip=$server_private_floatingIp" | tee --append ${HOME}/nscl_info.conf
@@ -9,6 +11,6 @@ echo "exports.ipnscl='$server_private';" | tee --append /home/ubuntu/OpenMTC-Chu
 #echo "exports.iplbaas='10.0.14.7';" | tee --append ${HOME}/OpenMTC-Chula/openmtc/settings/ipserv.js
 #Set IP NSCL
 #echo "exports.ipnscl='$nscl_private';" | tee --append ${HOME}/OpenMTC-Chula/openmtc/settings/ipserv.js
-sudo bash -c "echo exports.ipnip=\'`ifconfig eth0 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://'`\'\; >> /home/ubuntu/OpenMTC-Chula/openmtc/settings/ipserv.js"
-echo "exports.fipnip='$server_private_floatingIp';" | tee --append /home/ubuntu/OpenMTC-Chula/openmtc/settings/ipserv.js
+echo "exports.ipnip='$ipaddress';" | tee --append /home/ubuntu/OpenMTC-Chula/openmtc/settings/ipserv.js"
+echo "exports.fipnscl='$server_private_floatingIp';" | tee --append /home/ubuntu/OpenMTC-Chula/openmtc/settings/ipserv.js
 echo "exports.ipopenstack='192.168.9.14';" | tee --append /home/ubuntu/OpenMTC-Chula/openmtc/settings/ipserv.js
