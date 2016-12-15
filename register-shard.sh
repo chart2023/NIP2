a96b2392d5662bf8e4a1fdf4aed0bd145d878728
Apichart <chart2023@users.noreply.github.com>
2016-12-15 15:38:05 +0700

    Update register-shard.sh

--- register-shard.sh
+++ register-shard.sh
@@ -5,8 +5,21 @@ ipfile="./ipaddress.txt"
 ipaddress=$(head -1 $ipfile)
 sudo rm -rf /var/lib/mongod
 sudo mkdir /var/lib/mongod
-sudo mongod --shardsvr --replSet $REPLSET --dbpath /var/lib/mongod --fork --syslog --port 27017
-sleep 3
-mongo --port 27017 --eval "rs.initiate()"
-sleep 3
-mongo --host $ipaddress --port 27020 --eval "sh.addShard( '$REPLSET/$THISHOST:27017' )"
+service=mongos
+for i in {1..5}
+do
+        if (( $(ps -ef | grep -v grep | grep $service | wc -l) > 0 ))
+        then
+                echo "$service is running!!!"
+                sudo mongod --shardsvr --replSet $REPLSET --dbpath /var/lib/mongod --fork --syslog --port 27017
+                sleep 3
+                mongo --port 27017 --eval "rs.initiate()"
+                sleep 3
+                mongo --host $ipaddress --port 27020 --eval "sh.addShard( '$REPLSET/$THISHOST:27017' )"
+        else
+        echo "$service stopped:$i"
+        sleep 10
+        fi
+done
+
+
