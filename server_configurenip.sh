866e5d3fe7f05cc363144ecc739e19493f508833
Apichart <chart2023@users.noreply.github.com>
2016-12-18 00:32:39 +0700

    Update server_configurenip.sh

--- server_configurenip.sh
+++ server_configurenip.sh
@@ -5,15 +5,15 @@ sudo rm -rf ${HOME}/nscl_info.conf
 echo "nscl_ip=$server_private" | tee --append ${HOME}/nscl_info.conf
 echo "nscl_fip=$server_private_floatingIp" | tee --append ${HOME}/nscl_info.conf
 echo "nscl_hostname=$server_hostname" | tee --append ${HOME}/nscl_info.conf
-sudo rm -rf /home/ubuntu/OpenMTC-Chula/openmtc/settings/ipserv.js
+sudo rm -rf /OpenMTC-Chula/openmtc/settings/ipserv.js
 #SET IP LBaaS
-echo "exports.ipnscl='$server_private';" | tee --append /home/ubuntu/OpenMTC-Chula/openmtc/settings/ipserv.js
+echo "exports.ipnscl='$server_private';" | tee --append /OpenMTC-Chula/openmtc/settings/ipserv.js
 #echo "exports.iplbaas='10.0.14.7';" | tee --append ${HOME}/OpenMTC-Chula/openmtc/settings/ipserv.js
 #Set IP NSCL
 #echo "exports.ipnscl='$nscl_private';" | tee --append ${HOME}/OpenMTC-Chula/openmtc/settings/ipserv.js
-echo "exports.ipnip='$ipaddress';" | tee --append /home/ubuntu/OpenMTC-Chula/openmtc/settings/ipserv.js
-echo "exports.fipnscl='$server_private_floatingIp';" | tee --append /home/ubuntu/OpenMTC-Chula/openmtc/settings/ipserv.js
-echo "exports.ipopenstack='192.168.9.14';" | tee --append /home/ubuntu/OpenMTC-Chula/openmtc/settings/ipserv.js
+echo "exports.ipnip='$ipaddress';" | tee --append /OpenMTC-Chula/openmtc/settings/ipserv.js
+echo "exports.fipnscl='$server_private_floatingIp';" | tee --append /OpenMTC-Chula/openmtc/settings/ipserv.js
+echo "exports.ipopenstack='192.168.9.14';" | tee --append /OpenMTC-Chula/openmtc/settings/ipserv.js
 #######
 echo "STEP:GET IPLBAAS"
 user='chart'
@@ -26,4 +26,4 @@ expect /opt/openbaton/scripts/exchangekey.exp $keydata
 iplbaas=$(ssh $user@$host_os "./request_vip_lbaas.sh")
 echo "iplbaas="
 echo $iplbaas
-sudo echo "exports.iplbaas='$iplbaas';" | tee --append /home/ubuntu/OpenMTC-Chula/openmtc/settings/ipserv.js
+sudo echo "exports.iplbaas='$iplbaas';" | tee --append /OpenMTC-Chula/openmtc/settings/ipserv.js
