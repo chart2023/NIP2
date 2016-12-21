39a6a7a91b8d32ae475fa961e496aae510f29ad7
Apichart <chart2023@users.noreply.github.com>
2016-12-21 15:21:42 +0700

    Update start-nip.sh

--- start-nip.sh
+++ start-nip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "STEP: START NIP"
 user='ubuntu'
-dbhost='192.168.9.122'
+#dbhost='192.168.9.122'
 #wget http://192.168.9.14:8080/v1/AUTH_b8e61c4a0b1b4d2f82929563cab8c55a/openmtc/openstack_key14.pem --output-document=${HOME}/openstack_key14.pem
 #sudo chmod 600 ${HOME}/openstack_key14.pem
 #ssh -o StrictHostKeyChecking=no -i ${HOME}/openstack_key14.pem -l $user $dbhost --eval "mongo --port 27020 initshard.js"
