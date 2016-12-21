ad2ef8bdedc458a7eb28acaca5fc27e5e32df472
Apichart <chart2023@users.noreply.github.com>
2016-12-21 15:03:50 +0700

    Update client_configurenscl.sh

--- client_configurenscl.sh
+++ client_configurenscl.sh
@@ -6,7 +6,7 @@ echo "nip_fip=$client_private_floatingIp" | sudo tee --append ${HOME}/nip_info.c
 echo "nip_hostname=$client_hostname" | sudo tee --append ${HOME}/nip_info.conf
 echo "STEP: REGISTER LBAAS"
 user='chart'
-host_os='192.168.9.14'
+host_os='192.168.9.12'
 ipfile="./ipaddress.txt"
 ipaddress=$(head -1 $ipfile)
 ssh-keygen -N "" -f /root/.ssh/id_rsa
