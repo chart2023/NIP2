49135ac29ee4112f54c6869cab7042a1d66b41e4
Apichart <chart2023@users.noreply.github.com>
2016-12-22 20:53:52 +0700

    Update start-nip.sh

--- start-nip.sh
+++ start-nip.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-echo "STEP: START NIP"
+echo "START NIP"
+echo "START at:" $(date)
 sleep 30
 sudo nohup sudo node /OpenMTC-Chula/openmtc-NIP/ProxyGateway/NIP_IEEE1888_ETSI.js >/home/ubuntu/ieee.log 2>/home/ubuntu/ieee.err &
 sleep 10
 sudo nohup sudo node /OpenMTC-Chula/openmtc-NIP/ProxyGateway/NIP_ETSI_IEEE1888_nscl.js >/home/ubuntu/etsi.log 2>/home/ubuntu/etsi.err &
+echo "STOP at:" $(date)
+echo "##########FINISHED############"
