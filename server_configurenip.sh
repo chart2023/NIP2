5e90690e5e75010be3ab36187535944c55436a5d
Apichart <chart2023@users.noreply.github.com>
2016-12-22 15:11:42 +0700

    Update server_configurenip.sh

--- server_configurenip.sh
+++ server_configurenip.sh
@@ -23,7 +23,7 @@ keyfile="/root/.ssh/id_rsa.pub"
 keydata=$(cat $keyfile)
 echo $keydata
 expect /opt/openbaton/scripts/exchangekey.exp $keydata
-iplbaas=$(ssh $user@$host_os "./request_vip_lbaas.sh")
+iplbaas=$(ssh $user@$host_os "./request-vip-lbaas.sh")
 echo "iplbaas="
 echo $iplbaas
 sudo echo "exports.iplbaas='$iplbaas';" | tee --append /OpenMTC-Chula/openmtc/settings/ipserv.js
