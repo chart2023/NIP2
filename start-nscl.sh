ba563e9aa094085bd738b3738db9b2c671d00208
Apichart <chart2023@users.noreply.github.com>
2016-12-22 10:36:44 +0700

    Update start-nscl.sh

--- start-nscl.sh
+++ start-nscl.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 user='ubuntu'
-dbhost='192.168.9.122'
+source /root/db_info.conf
+dbhost=$db_ip
+#dbhost='192.168.9.122'
 nohup node /OpenMTC-Chula/nscl >/home/ubuntu/nscl.log 2>/home/ubuntu/nscl.err &
 sleep 5
 expect /opt/openbaton/scripts/init-shard.exp $dbhost
