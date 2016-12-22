a5423692c4602f70637f8e68dc8fdf3fcf528980
Apichart <chart2023@users.noreply.github.com>
2016-12-22 20:46:32 +0700

    Update get-iplbaas.sh

--- get-iplbaas.sh
+++ get-iplbaas.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "STEP:GET IPLBAAS"
+echo "START at:" $(date)
 user='chart'
 host_os='192.168.9.12'
 ssh-keygen -N "" -f /root/.ssh/id_rsa
@@ -11,3 +12,5 @@ iplbaas=$(ssh $user@$host_os "./request-vip-lbaas.sh")
 echo "iplbaas="
 echo $iplbaas
 sudo echo "exports.iplbaas='$iplbaas';" | tee --append /OpenMTC-Chula/openmtc/settings/ipserv.js
+echo "STOP at:" $(date)
+echo "##########FINISHED############"
