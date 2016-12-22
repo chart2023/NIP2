6f76027fa9c98fad4a7857c2109a9d60f1b426d3
Apichart <chart2023@users.noreply.github.com>
2016-12-22 23:49:03 +0700

    Update start-nip.sh

--- start-nip.sh
+++ start-nip.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 echo "START NIP"
 echo "START at:" $(date)
+source /root/nscl_info.conf
+NSCL_IP=$nscl_ip
 sleep 30
-sudo nohup sudo node /OpenMTC-Chula/openmtc-NIP/ProxyGateway/NIP_IEEE1888_ETSI.js >/home/ubuntu/ieee.log 2>/home/ubuntu/ieee.err &
-sleep 10
-sudo nohup sudo node /OpenMTC-Chula/openmtc-NIP/ProxyGateway/NIP_ETSI_IEEE1888_nscl.js >/home/ubuntu/etsi.log 2>/home/ubuntu/etsi.err &
+for i in {1..10}
+do
+        nc -z -v $NSCL_IP 15000
+        if [ $? -eq 0 ];
+        then
+                sudo nohup sudo node /OpenMTC-Chula/openmtc-NIP/ProxyGateway/NIP_IEEE1888_ETSI.js >/home/ubuntu/ieee.log 2>/home/ubuntu/ieee.err &
+                sleep 10
+                sudo nohup sudo node /OpenMTC-Chula/openmtc-NIP/ProxyGateway/NIP_ETSI_IEEE1888_nscl.js >/home/ubuntu/etsi.log 2>/home/ubuntu/etsi.err &
+                break
+        else
+        echo "NSCL stopped:$i on start_nscl"
+        sleep 15
+        fi
+done
 echo "STOP at:" $(date)
 echo "##########FINISHED############"
