738384232fd494b312866c92afc0d0d0aeb70408
Apichart <chart2023@users.noreply.github.com>
2016-12-22 23:43:06 +0700

    Update start-nscl.sh

--- start-nscl.sh
+++ start-nscl.sh
@@ -5,8 +5,20 @@ user='ubuntu'
 source /root/db_info.conf
 dbhost=$db_ip
 #dbhost='192.168.9.122'
-nohup node /OpenMTC-Chula/nscl >/home/ubuntu/nscl.log 2>/home/ubuntu/nscl.err &
-sleep 5
-expect /opt/openbaton/scripts/init-shard.exp $dbhost
+
 echo "STOP at:" $(date)
 echo "##########FINISHED############"
+for i in {1..5}
+do
+        nc -z -v $dbhost 27017
+        if [ $? -eq 0 ];
+        then
+                nohup node /OpenMTC-Chula/nscl >/home/ubuntu/nscl.log 2>/home/ubuntu/nscl.err &
+                sleep 5
+                expect /opt/openbaton/scripts/init-shard.exp $dbhost
+                break
+        else
+        echo "Mongos stopped:$i on start_nscl"
+        sleep 15
+        fi
+done
