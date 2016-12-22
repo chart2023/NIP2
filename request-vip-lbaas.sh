743c2395590c4a468d5cb413bda3526de5322cb5
Apichart <chart2023@users.noreply.github.com>
2016-12-22 20:52:35 +0700

    Update request-vip-lbaas.sh

--- request-vip-lbaas.sh
+++ request-vip-lbaas.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 echo "STEP: REquest vip lbaas"
+echo "START at:" $(date)
 source ${HOME}/devstack/openrc admin admin >/dev/null
 #neutron lbaas-loadbalancer-create --name lb1 private_subnet
 #neutron lbaas-loadbalancer-show lb1
 #neutron lbaas-listener-create --loadbalancer lb1 --protocol HTTP --protocol-port 15000 --name listener1
 #neutron lbaas-pool-create --lb-algorithm ROUND_ROBIN --listener listener1 --protocol HTTP --name pool1
 neutron lbaas-loadbalancer-show 'Load Balancer 1' | awk '$2 == "vip_address" {print $4}'
+echo "STOP at:" $(date)
+echo "##########FINISHED############"
