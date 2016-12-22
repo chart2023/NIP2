d2ff1e8c90179420f3009bab26a3a0eed3208fbf
Apichart <chart2023@users.noreply.github.com>
2016-12-22 20:45:06 +0700

    Update db_configure.sh

--- db_configure.sh
+++ db_configure.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-echo "db configure"
+echo "START at:" $(date)
+echo "db configure at NSCL"
 echo "db_ip=$db_private" | sudo tee --append ${HOME}/db_info.conf
 echo "db_fip=$db_private_floatingIp" | sudo tee --append ${HOME}/db_info.conf
 echo "db_hostname=$db_hostname" | sudo tee --append ${HOME}/db_info.conf
 echo "exports.ipdb='$db_private';" | tee --append /OpenMTC-Chula/openmtc/settings/ipserv.js
+echo "STOP at:" $(date)
+echo "##########FINISHED############"
