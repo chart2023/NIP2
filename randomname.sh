4e11cba714f5df2d901a772b0363a6d51787553f
Apichart <chart2023@users.noreply.github.com>
2016-12-22 20:47:50 +0700

    Update randomname.sh

--- randomname.sh
+++ randomname.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+echo "START at:" $(date)
 REPLSET=$(cat /dev/urandom | tr -dc 'A-Z' | fold -w 3 | head -n 1)
 echo $REPLSET >> /root/db_info.conf
+echo "STOP at:" $(date)
+echo "##########FINISHED############"
