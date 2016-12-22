7fd1974d4ec58e37b2dee078646c10fef5f86f83
Apichart <chart2023@users.noreply.github.com>
2016-12-22 20:45:47 +0700

    Update client_configurenscl.sh

--- client_configurenscl.sh
+++ client_configurenscl.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "#############################"
 echo "client configure at NSCL"
+echo "START at:" $(date)
 echo "nip_ip=$client_private" | sudo tee --append ${HOME}/nip_info.conf
 echo "nip_fip=$client_private_floatingIp" | sudo tee --append ${HOME}/nip_info.conf
 echo "nip_hostname=$client_hostname" | sudo tee --append ${HOME}/nip_info.conf
