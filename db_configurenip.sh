3cec030adf96d6e2c043fe918e2be177e38f7971
Apichart <chart2023@users.noreply.github.com>
2016-12-22 20:46:30 +0700

    Update db_configurenip.sh

--- db_configurenip.sh
+++ db_configurenip.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "db configure at NIP"
+echo "START at:" $(date)
 echo "db_ip=$db_private" | sudo tee --append ${HOME}/db_info.conf
 echo "db_fip=$db_private_floatingIp" | sudo tee --append ${HOME}/db_info.conf
 echo "db_hostname=$db_hostname" | sudo tee --append ${HOME}/db_info.conf
@@ -9,3 +10,5 @@ source ${HOME}/db_info.conf
 dbhost=$db_ip
 expect /opt/openbaton/scripts/setup-shard.exp $dbhost
 echo "#############FINISHED################"
+echo "STOP at:" $(date)
+echo "##########FINISHED############"
