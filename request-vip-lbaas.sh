c068218473fd3c542ec4c456265b9b4364fced39
Apichart <chart2023@users.noreply.github.com>
2016-12-22 15:07:53 +0700

    Update request-vip-lbaas.sh

--- request-vip-lbaas.sh
+++ request-vip-lbaas.sh
@@ -5,4 +5,4 @@ source ${HOME}/devstack/openrc admin admin >/dev/null
 #neutron lbaas-loadbalancer-show lb1
 #neutron lbaas-listener-create --loadbalancer lb1 --protocol HTTP --protocol-port 15000 --name listener1
 #neutron lbaas-pool-create --lb-algorithm ROUND_ROBIN --listener listener1 --protocol HTTP --name pool1
-neutron lbaas-loadbalancer-show lb1 | awk '$2 == "vip_address" {print $4}'
+neutron lbaas-loadbalancer-show 'Load Balancer 1' | awk '$2 == "vip_address" {print $4}'
