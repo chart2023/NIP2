712d204909f8317561a547fcfa42516c1b70d682
Apichart <chart2023@users.noreply.github.com>
2016-12-22 11:33:41 +0700

    Update db_configure.sh

--- db_configure.sh
+++ db_configure.sh
@@ -4,6 +4,3 @@ echo "db_ip=$db_private" | sudo tee --append ${HOME}/db_info.conf
 echo "db_fip=$db_private_floatingIp" | sudo tee --append ${HOME}/db_info.conf
 echo "db_hostname=$db_hostname" | sudo tee --append ${HOME}/db_info.conf
 echo "exports.ipdb='$db_private';" | tee --append /OpenMTC-Chula/openmtc/settings/ipserv.js
-source ${HOME}/db_info.conf
-dbhost=$db_ip
-expect /opt/openbaton/scripts/setup-shard.exp $dbhost
