42823c194bf5ca792406c7e36d873750325a18fa
Apichart <chart2023@users.noreply.github.com>
2016-12-22 10:45:38 +0700

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "client configure"
 echo "nip_ip=$client_private" | sudo tee --append ${HOME}/nip_info.conf
+echo "nip_ip2=${client_private}" | sudo tee --append ${HOME}/nip_info.conf
