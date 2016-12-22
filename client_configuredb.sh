0311d238a606708c62cd8ba81629d08003fe2fc8
Apichart <chart2023@users.noreply.github.com>
2016-12-23 00:57:27 +0700

    Update client_configuredb.sh

--- client_configuredb.sh
+++ client_configuredb.sh
@@ -21,7 +21,6 @@ echo "STEP: REGISTER SHARD"
 THISHOST=$(hostname)
 REPLSET=$(head -1 ${HOME}/db_info.conf)
 sudo rm -rf /var/lib/mongod
-
 #sudo mongod --shardsvr --replSet $REPLSET --dbpath /var/lib/mongod --fork --syslog --port 27017
 #sleep 3
 #mongo --port 27017 --eval "rs.initiate()"
