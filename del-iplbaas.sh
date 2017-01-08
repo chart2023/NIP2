8cfb1cc7e054ea3ed68a9dae81c5706032192aa5
Apichart <chart2023@users.noreply.github.com>
2017-01-08 11:06:16 +0700

    Update del-iplbaas.sh

--- del-iplbaas.sh
+++ del-iplbaas.sh
@@ -4,6 +4,7 @@ echo "delete iplbaas at Openstack"
 echo "START at:" $(date)
 host_os='192.168.9.12'
 ipfile="./ipaddress.txt"
+user='chart'
 ipaddress=$(head -1 $ipfile)
 ssh $user@$host_os "./delete-iplbaas.sh $ipaddress"
 echo "STOP at:" $(date)
