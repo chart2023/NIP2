c11332e5f463cd5834f4e16a2b6bcdccda69ff9e
Apichart <chart2023@users.noreply.github.com>
2017-01-09 11:13:12 +0700

    Update db_configure.sh

--- db_configure.sh
+++ db_configure.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "START at:" $(date)
-echo "db configure at NSCL"
+echo "db configure at "
+echo $(hostname)
 echo "db_ip=$db_private" | sudo tee --append ${HOME}/db_info.conf
 echo "db_fip=$db_private_floatingIp" | sudo tee --append ${HOME}/db_info.conf
 echo "db_hostname=$db_hostname" | sudo tee --append ${HOME}/db_info.conf
