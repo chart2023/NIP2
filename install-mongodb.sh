897d6427dc9b7b1b87751d850f7450f8943e037c
Apichart <chart2023@users.noreply.github.com>
2017-01-23 10:44:33 +0700

    Update install-mongodb.sh

--- install-mongodb.sh
+++ install-mongodb.sh
@@ -19,7 +19,7 @@ else
         echo "Server cannot connect to Internet"
 fi
 ntpq -p
-cp /opt/openbaton/scripts/initshard.js /home/ubuntu/initshard.js
+cp /opt/openbaton/scripts/initshard_bigfile.js /home/ubuntu/initshard.js
 chown ubuntu:ubuntu /home/ubuntu/initshard.js
 chmod 755 /home/ubuntu/initshard.js
 echo "STOP at:" $(date)
