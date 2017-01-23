b0d643b331f3f77f8ed9def52526cc6f81fe1fa9
Apichart <chart2023@users.noreply.github.com>
2017-01-23 15:00:25 +0700

    Update db_configure.sh

--- db_configure.sh
+++ db_configure.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "START at:" $(date)
-echo "db configure at "
+echo "DB configure at "
 echo $(hostname)
 echo "db_ip=$db_private" | sudo tee --append ${HOME}/db_info.conf
 echo "db_fip=$db_private_floatingIp" | sudo tee --append ${HOME}/db_info.conf
 echo "db_hostname=$db_hostname" | sudo tee --append ${HOME}/db_info.conf
 echo "exports.ipdb='$db_private';" | tee --append /OpenMTC-Chula/openmtc/settings/ipserv.js
-echo "STOP at:" $(date)
+echo "FINISHED at:" $(date)
 echo "##########FINISHED############"
