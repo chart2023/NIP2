823bbc412a66a119a29d3b73ef7fef9c6587961d
Apichart <chart2023@users.noreply.github.com>
2017-01-23 13:07:06 +0700

    Update db_configurenip.sh

--- db_configurenip.sh
+++ db_configurenip.sh
@@ -7,8 +7,15 @@ echo "db_hostname=$db_hostname" | sudo tee --append ${HOME}/db_info.conf
 echo "exports.ipdb='$db_private';" | tee --append /OpenMTC-Chula/openmtc/settings/ipserv.js
 echo "$db_private" | sudo tee --append /home/ubuntu/maindb_info.conf
 source ${HOME}/db_info.conf
-dbhost=$db_ip
-expect /opt/openbaton/scripts/setup-shard.exp $dbhost
+MAINDB_IP=$(head -1 /home/ubuntu/maindb_info.conf)
+if [ $MAINDB_IP = $db_ip];
+then
+  dbhost=$db_ip
+  expect /opt/openbaton/scripts/setup-shard.exp $dbhost
+else
+  echo "THIS IS EXTENDED DB"
+  break;
+fi
 echo "#############FINISHED################"
 echo "STOP at:" $(date)
 echo "##########FINISHED############"
