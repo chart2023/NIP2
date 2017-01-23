fa4478935b3082607cae85f41461a96c9fac4fea
Apichart <chart2023@users.noreply.github.com>
2017-01-23 12:19:41 +0700

    Update client_configuredb.sh

--- client_configuredb.sh
+++ client_configuredb.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+THISHOST=$(hostname)
+ipfile="./ipaddress.txt"
+THISIP=$(head -1 $ipfile)
 echo "client configure at DB-SERVER"
 echo "START at:" $(date)
 echo "nip_ip=$client_private" | sudo tee --append ${HOME}/nip_info.conf
@@ -17,34 +20,60 @@ do
                 break
         fi
 done
-echo "STEP: REGISTER SHARD"
-THISHOST=$(hostname)
-REPLSET=$(head -1 ${HOME}/db_info.conf)
-sudo rm -rf /var/lib/mongod
-#sudo mongod --shardsvr --replSet $REPLSET --dbpath /var/lib/mongod --fork --syslog --port 27017
-#sleep 3
-#mongo --port 27017 --eval "rs.initiate()"
-#sleep 3
-#mongo --host $ipaddress --port 27020 --eval "sh.addShard( '$REPLSET/$THISHOST:27017' )"
 SERVICE='mongos'
-MYHOST="$REPLSET/$THISHOST:27017"
-for i in {1..5}
-do
-        nc -z -v $MAINDB_IP 27020
-        if [ $? -eq 0 ];
-        then
-                echo "$SERVICE is running!!!"
-                sudo mkdir /var/lib/mongod
-                sudo mongod --shardsvr --replSet $REPLSET --dbpath /var/lib/mongod --fork --syslog --port 27017
-                sleep 3
-                mongo --port 27017 --eval "rs.initiate()"
-                sleep 3
-                mongo --host $MAINDB_IP --port 27020 --eval "sh.addShard( '$MYHOST' )"
-                break
-        else
-        echo "$service stopped:$i on client_configuredb.sh"
-        sleep 15
-        fi
-done
-echo "STOP at:" $(date)
+if [ $MAINDB_IP = $THISIP];
+then
+        echo "STEP: REGISTER MAIN SHARD"
+        REPLSET=$(head -1 /home/ubuntu/db_info.conf)
+        sudo rm -rf /var/lib/mongod
+        #sudo mongod --shardsvr --replSet $REPLSET --dbpath /var/lib/mongod --fork --syslog --port 27017
+        #sleep 3
+        #mongo --port 27017 --eval "rs.initiate()"
+        #sleep 3
+        #mongo --host $ipaddress --port 27020 --eval "sh.addShard( '$REPLSET/$THISHOST:27017' )"
+        MYHOST="$REPLSET/$THISHOST:27017"
+        for i in {1..5}
+        do
+                nc -z -v $MAINDB_IP 27020
+                if [ $? -eq 0 ];
+                then
+                        echo "$SERVICE is running!!!"
+                        sudo mkdir /var/lib/mongod
+                        sudo mongod --shardsvr --replSet $REPLSET --dbpath /var/lib/mongod --fork --syslog --port 27017
+                        sleep 3
+                        mongo --port 27017 --eval "rs.initiate()"
+                        sleep 3
+                        mongo --host $MAINDB_IP --port 27020 --eval "sh.addShard( '$MYHOST' )"
+                        break
+                else
+                echo "$service stopped:$i on client_configuredb.sh"
+                sleep 15
+                fi
+        done
+        echo "FINISHED at:" $(date)
+else
+        echo "STEP: REGISTER extended SHARD"
+        MAINREPLSET=$(ssh -o StrictHostKeyChecking=no -i /openstack_key.pem -l ubuntu $MAINDB_IP "sudo head -1 /home/ubuntu/db_info.conf")
+        sudo rm -rf /var/lib/mongod
+        MYHOST="$MAINREPLSET/$THISHOST:27017"
+        for i in {1..5}
+        do
+                nc -z -v $MAINDB_IP 27020
+                if [ $? -eq 0 ];
+                then
+                        echo "$SERVICE is running!!!"
+                        sudo mkdir /var/lib/mongod
+                        sudo mongod --shardsvr --replSet $MAINREPLSET --dbpath /var/lib/mongod --fork --syslog --port 27017
+                        sleep 3
+                        mongo --port 27017 --eval "rs.initiate()"
+                        sleep 3
+                        mongo --host $MAINDB_IP --port 27020 --eval "sh.addShard( '$MYHOST' )"
+                        break
+                else
+                echo "$service stopped:$i on client_configuredb.sh"
+                sleep 15
+                fi
+        done
+        echo "FINISHED at:" $(date)
+fi
 echo "##########FINISHED############"
