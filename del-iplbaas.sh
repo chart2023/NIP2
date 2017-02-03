507210b1284bc5451e94be86ea0487303b7e2830
Apichart <chart2023@users.noreply.github.com>
2017-02-03 10:58:21 +0700

    Update del-iplbaas.sh

--- del-iplbaas.sh
+++ del-iplbaas.sh
@@ -6,6 +6,7 @@ host_os='192.168.9.12'
 ipfile="./ipaddress.txt"
 user='chart'
 ipaddress=$(head -1 $ipfile)
+echo "delete iplbaas:" $ipaddress
 ssh $user@$host_os "./delete-iplbaas.sh $ipaddress"
 echo "STOP at:" $(date)
 echo "##########FINISHED############"
