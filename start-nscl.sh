0845c3c2b375aab44788a32577bf4bd2ac4c170d
Apichart <chart2023@users.noreply.github.com>
2017-02-05 11:48:45 +0700

    Update start-nscl.sh

--- start-nscl.sh
+++ start-nscl.sh
@@ -6,7 +6,6 @@ source /root/db_info.conf
 dbhost=$db_ip
 #dbhost='192.168.9.122'
 user1='chart'
-host_os='192.168.9.12'
 ipfile="./ipaddress.txt"
 ipaddress=$(head -1 $ipfile)
 
@@ -32,24 +31,24 @@ do
                          | python -m  json.tool > token.info
                         TOKEN=$(cat token.info |  jq '.access.token.id' | tr -d '"')
                         echo "TOKEN:"$TOKEN
-                        curl -s -X GET http://192.168.9.12:9696/v2.0/subnets?name=private_subnet \
+                        curl -s -X GET http://$ipopenstack:9696/v2.0/subnets?name=private_subnet \
                          -H "X-Auth-Token: $TOKEN" \
                          | python -m json.tool > subnet.info
                         subnet=$(cat subnet.info | jq '.subnets[].id' | tr -d '"')
                         echo $subnet
-                        curl -s -X GET http://192.168.9.12:9696/v2.0/lbaas/loadbalancers?name=loadbalancer1 \
+                        curl -s -X GET http://$ipopenstack:9696/v2.0/lbaas/loadbalancers?name=loadbalancer1 \
                          -H "X-Auth-Token: $TOKEN" \
                          | python -m json.tool > lbaas.txt
                         vip=$(cat lbaas.txt | jq '.loadbalancers[].vip_address' | tr -d '"')
                         pools=$(cat lbaas.txt | jq '.loadbalancers[].pools[].id' | tr -d '"')
                         echo $vip
                         echo $pools
-                        curl -s -X POST http://192.168.9.12:9696/v2.0/lbaas/pools/$pools/members \
+                        curl -s -X POST http://$ipopenstack:9696/v2.0/lbaas/pools/$pools/members \
                          -H "X-Auth-Token: $TOKEN" \
                          -d '{"member": {"address": "'"$ipaddress"'", "name": "'"$ipaddress"'","subnet_id": "'"$subnet"'", "protocol_port": "15000"}}'
-                        curl -s -X GET http://192.168.9.12:9696/v2.0/lbaas/pools/$pools/members \
+                        curl -s -X GET http://$ipopenstack:9696/v2.0/lbaas/pools/$pools/members \
                         -H "X-Auth-Token: $TOKEN" | python -m json.tool
-                        ssh-keygen -f "/home/chart/.ssh/known_hosts" -R 192.168.9.122
+                        ssh $user1@$host_os "./add-iplbaas.sh $ipopenstack
                         break
                         else
                         echo "NSCL is not working on NSCL"
