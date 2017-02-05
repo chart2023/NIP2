d992f27eb8709e199343a0a72124b99325c1f49a
Apichart <chart2023@users.noreply.github.com>
2017-02-05 14:18:50 +0700

    Update server_configurenip.sh

--- server_configurenip.sh
+++ server_configurenip.sh
@@ -18,12 +18,21 @@ echo "exports.fipnscl='$server_private_floatingIp';" | tee --append /OpenMTC-Chu
 #echo "exports.ipopenstack='161.200.90.78';" | tee --append /OpenMTC-Chula/openmtc/settings/ipserv.js
 #############
 echo "STEP:GET IPLBAAS"
-
-
-
-
-
-
+source /home/ubuntu/openstack.conf
+echo $OS_AUTH_URL
+echo $OS_TOKEN
+echo $OS_USERNAME
+echo $OS_PASSWORD
+curl -s -X POST $OS_AUTH_URL/tokens \
+-H "Content-Type: application/json" \
+-d '{"auth": {"tenantName": "admin", "passwordCredentials": {"username": "'"$OS_USERNAME"'", "password": "'"$OS_PASSWORD"'"}}}'  \
+ | python -m json.tool > token.txt
+TOKEN=$(cat token.txt |  jq '.access.token.id' | tr -d '"')
+echo $TOKEN
+curl -s -X GET http://192.168.9.12:9696/v2.0/lbaas/loadbalancers.json?fields=id \
+ -H "X-Auth-Token: $TOKEN" \
+ | python -m json.tool > lbaas.txt
+viplbaas=$(cat lbaas.txt | jq '.loadbalancers[].vip_address' | tr -d '"')
 user='chart'
 host_os='192.168.9.12'
 ssh-keygen -N "" -f /root/.ssh/id_rsa
@@ -31,8 +40,8 @@ keyfile="/root/.ssh/id_rsa.pub"
 keydata=$(cat $keyfile)
 echo $keydata
 expect /opt/openbaton/scripts/exchangekey.exp $keydata
-iplbaas=$(ssh $user@$host_os "./request-vip-lbaas.sh")
-echo "iplbaas=$iplbaas" | tee --append ${HOME}/nscl_info.conf
-sudo echo "exports.iplbaas='$iplbaas';" | tee --append /OpenMTC-Chula/openmtc/settings/ipserv.js
+#iplbaas=$(ssh $user@$host_os "./request-vip-lbaas.sh")
+echo "iplbaas=$viplbaas" | tee --append ${HOME}/nscl_info.conf
+sudo echo "exports.iplbaas='$viplbaas';" | tee --append /OpenMTC-Chula/openmtc/settings/ipserv.js
 echo "FINISHED at:" $(date)
 echo "##########FINISHED############"
