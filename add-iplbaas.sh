7131b13766ee0a5b471f5b0459edb9592a9abee2
Apichart <chart2023@users.noreply.github.com>
2017-02-05 15:13:02 +0700

    Update add-iplbaas.sh

--- add-iplbaas.sh
+++ add-iplbaas.sh
@@ -3,5 +3,5 @@ echo "START at:" $(date) "by" $(hostname)
 echo "add iplbaas"
 ipaddress=$1
 echo "ADDED:" $ipaddress at $(date) >> log_iplbaas.log
-echo $ipaddress >> {HOME}/iplbaas.info
+echo $ipaddress >> ${HOME}/iplbaas.info
 echo "FINISHED at:" $(date)
