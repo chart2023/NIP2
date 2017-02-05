e83300a96da9254e3615c9072ad8012fecc67841
Apichart <chart2023@users.noreply.github.com>
2017-02-05 20:48:36 +0700

    Update scale_in_lbaas.sh

--- scale_in_lbaas.sh
+++ scale_in_lbaas.sh
@@ -4,6 +4,7 @@ echo "delete iplbaas at Openstack"
 echo "START at:" $(date)
 ipfile="./ipaddress.txt"
 ipaddress=$(head -1 $ipfile)
+source /root/nip_info.conf
 echo "delete iplbaas:" $ipaddress
 ssh -o StrictHostKeyChecking=no -i /openstack_key.pem -l ubuntu $nip_ip "/opt/openbaton/scripts/del_iplbaas_scale.sh $ipaddress"
 echo "STOP at:" $(date)
