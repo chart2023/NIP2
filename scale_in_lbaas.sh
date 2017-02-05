df6946bd702e2e5750ec95a4897f0ec538f22635
Apichart <chart2023@users.noreply.github.com>
2017-02-05 21:25:26 +0700

    Update scale_in_lbaas.sh

--- scale_in_lbaas.sh
+++ scale_in_lbaas.sh
@@ -6,6 +6,6 @@ ipfile="./ipaddress.txt"
 ipaddress=$(head -1 $ipfile)
 source /root/nip_info.conf
 echo "delete iplbaas:" $ipaddress
-ssh -o StrictHostKeyChecking=no -i /openstack_key.pem -l ubuntu $nip_ip "/opt/openbaton/scripts/del_iplbaas_scale.sh $ipaddress"
+ssh -o StrictHostKeyChecking=no -i /openstack_key.pem -l ubuntu $nip_ip "/home/ubuntu/del_iplbaas_scale.sh $ipaddress"
 echo "STOP at:" $(date)
 echo "##########FINISHED############"
