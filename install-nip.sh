7d38415862eb6d44d9f35e581da0ec3d92163f2b
Apichart <chart2023@users.noreply.github.com>
2017-02-05 14:17:35 +0700

    Update install-nip.sh

--- install-nip.sh
+++ install-nip.sh
@@ -20,6 +20,7 @@ chmod ugo+x /etc/init.d/start-nip.sh
 update-rc.d start-nip.sh defaults
 sudo rm -rf /OpenMTC-Chula/openmtc/settings/ipserv.js
 echo "exports.ipopenstack='${publicip}';" | tee --append /OpenMTC-Chula/openmtc/settings/ipserv.js
+cp /opt/openbaton/scripts/openstack.conf /home/ubuntu/openstack.conf
 ntpq -p
 echo "STOP at:" $(date)
 echo "##########FINISHED############"
